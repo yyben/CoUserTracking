6bd837e7f2ff45ef0b98cd4cceabc0bebcb5bdb6
yyben <yyben@caece.net>
2016-05-29 14:22:50 +0800

    first update

---
+++ main.js
@@ -0,0 +1,41 @@
+var fname='lecture_actions_test.csv';
+
+// var finished = function(error, results) {
+//   console.log(results);
+// }
+
+// var foo = function(i, callback) {
+//   console.log(i);
+//   callback(null, i);  // first argument is error reason, second is result
+// }
+
+// queue(2)
+//   .defer(foo, 1)
+//   .defer(foo, 2)
+//   .defer(foo, 3)
+//   .defer(foo, 4)
+//   .awaitAll(finished); 
+
+// var foo = function(i, callback) {
+//   console.log(i);
+//   callback(null, i);  // first argument is error reason, second is result
+// }
+
+// console.log(fnames)
+// queue(2) //load multiple files
+//   .defer(d3.csv, fnames[0])
+//   .defer(d3.csv, fnames[1])
+//   .defer(d3.csv, fnames[2])
+//   .defer(d3.csv, fnames[3])
+//   .await(parseData);
+parseData();
+
+function parseData(fname){
+ 	d3.csv(fname,function(d){
+ 		console.log(d)
+
+
+ 	});
+
+
+ }
